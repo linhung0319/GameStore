655593f8ab6f39b2062c0c60429b96c0459b8261
linhung0319 <linhung0319@gmail.com>
2024-10-02 23:36:18 +0800

    Create data transfer objects (DTOs) for games

--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -1,6 +1,30 @@
+using GameStore.Api.Dtos;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+List<GameDto> games = [
+    new (
+        1,
+        "Street Fighter II",
+        "Fighting",
+        19.99M,
+        new DateOnly(1992, 7, 15)),
+    new (
+        2,
+        "Final Fantasy XIV",
+        "Roleplaying",
+        59.99M,
+        new DateOnly(2010, 9, 30)),
+    new (
+        3,
+        "FIFA 23",
+        "Sports",
+        69.99M,
+        new DateOnly(2022, 9, 27))
+];
+
+// GET /games
+app.MapGet("games", () => games);
 
 app.Run();
