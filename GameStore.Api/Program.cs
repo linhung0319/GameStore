2096d66214793107d85dde2c61fed6e0db48931f
linhung0319 <linhung0319@gmail.com>
2024-10-03 19:47:04 +0800

    Implement CRUD endpoints

--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -1,30 +1,8 @@
-using GameStore.Api.Dtos;
+using GameStore.Api.Endpoints;
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-List<GameDto> games = [
-    new (
-        1,
-        "Street Fighter II",
-        "Fighting",
-        19.99M,
-        new DateOnly(1992, 7, 15)),
-    new (
-        2,
-        "Final Fantasy XIV",
-        "Roleplaying",
-        59.99M,
-        new DateOnly(2010, 9, 30)),
-    new (
-        3,
-        "FIFA 23",
-        "Sports",
-        69.99M,
-        new DateOnly(2022, 9, 27))
-];
-
-// GET /games
-app.MapGet("games", () => games);
+app.MapGamesEndpoints();
 
 app.Run();
