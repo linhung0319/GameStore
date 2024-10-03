07c0e8c3ed5245ca7bc74bd8bfe3fa24b050808a
linhung0319 <linhung0319@gmail.com>
2024-10-03 21:43:04 +0800

    Handle invalid inputs

--- GameStore.Api/Dtos/CreateGameDto.cs
+++ GameStore.Api/Dtos/CreateGameDto.cs
@@ -1,8 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace GameStore.Api.Dtos;
 
 public record class CreateGameDto(
-    string Name,
-    string Genre,
-    decimal Price,
+    [Required][StringLength(50)] string Name,
+    [Required][StringLength(50)] string Genre,
+    [Range(1, 100)] decimal Price,
     DateOnly ReleaseDate
 );
